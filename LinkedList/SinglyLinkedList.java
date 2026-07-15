7428b1cba52106c74e7585d2eeb43d293ae111ab
Shiv Upadhyay <160642007+shiv8279@users.noreply.github.com>
2026-07-15 15:58:05 +0530

    Add SinglyLinkedList class with basic operations
    Implement a singly linked list with insert and display methods.

---
+++ LinkedList/SinglyLinkedList.java
@@ -0,0 +1,68 @@
+class Node{
+
+    int data;
+    Node next;
+
+    Node(int data){
+
+        this.data=data;
+        this.next=null;
+
+    }
+
+}
+
+public class SinglyLinkedList {
+
+    Node head;
+
+    public void insert(int data){
+
+        Node newNode=new Node(data);
+
+        if(head==null){
+
+            head=newNode;
+            return;
+
+        }
+
+        Node temp=head;
+
+        while(temp.next!=null){
+
+            temp=temp.next;
+
+        }
+
+        temp.next=newNode;
+
+    }
+
+    public void display(){
+
+        Node temp=head;
+
+        while(temp!=null){
+
+            System.out.print(temp.data+" ");
+
+            temp=temp.next;
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        SinglyLinkedList list=new SinglyLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        list.display();
+
+    }
+
+}
